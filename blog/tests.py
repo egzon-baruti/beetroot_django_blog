9b2d8b54b709ccc53ab2dbd5fe66620435d7415b
egzon-baruti <egzon.baruti@tive.com>
2023-12-30 11:02:46 +0100

    done

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,81 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse 
 
-# Create your tests here.
+from .models import Post
+
+
+
+class BlogTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user = get_user_model().objects.create_user(
+            username='testuser',
+            email = 'test@mail.com',
+            password = 'secret',
+        )
+
+        cls.post = Post.objects.create(
+            title = 'Sample Title',
+            body = 'Sample Body',
+            author = cls.user,
+        )
+    
+    def test_post_model(self):
+        self.assertEqual(self.post.title, "Sample Title")
+        self.assertEqual(self.post.body, 'Sample Body')
+        self.assertEqual(self.post.author.username,'testuser')
+        self.assertEqual(str(self.post), 'Sample Title')
+        self.assertEqual(self.post.get_absolute_url(),'/post/1/')
+
+    def test_url_exists_at_correct_location_listview(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_exists_at_correct_location_detailview(self):
+        response = self.client.get('/post/1/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_post_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response, "Sample Body")
+        self.assertTemplateUsed(response,'home.html')
+
+    def test_post_detailview(self):
+        response = self.client.get(reverse('post_detail', kwargs={'pk':self.post.pk}))
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response,'Sample Title')
+        self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_createview(self):
+        response = self.client.post(
+            reverse('post_new'),
+            {
+                'title':'TestTitle',
+                'body':'TestBody',
+                'author':self.user.id,
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'TestTitle')
+        self.assertEqual(Post.objects.last().body, 'TestBody')
+
+    def test_post_updateview(self):
+        response = self.client.post(
+            reverse('post_edit',args="1"),
+            {
+                'title':'TitleUpdate',
+                'body':'BodyUpdate',
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'TitleUpdate')
+        self.assertEqual(Post.objects.last().body, 'BodyUpdate')
+
+    def test_post_deleteview(self):
+        response = self.client.post(reverse('post_delete', args="1"))
+        self.assertEqual(response.status_code, 302)
